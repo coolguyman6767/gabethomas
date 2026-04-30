af7a0660779b38c381c531c4ce2dcc53b0c911e5
coolguyman6767 <cool.very.awesome@gmail.com>
2026-04-30 18:35:48 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,3 @@
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    let mySprite: Sprite = null
-    scene.cameraFollowSprite(mySprite)
     music.play(music.createSoundEffect(WaveShape.Sine, 512, 0, 155, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
 })
