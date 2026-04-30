6d3cb445899fcea703230330a70b833b006eedd4
thomas2stewart2-pixel <thomas2.stewart2@gmail.com>
2026-04-30 18:33:35 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,5 @@
-
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    let mySprite: Sprite = null
+    scene.cameraFollowSprite(mySprite)
+    music.play(music.createSoundEffect(WaveShape.Sine, 512, 0, 155, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+})
